eb9d61377f26ce1f3c53cf6692aac9d22883df22
licsber <admin@licsber.site>
2023-03-19 01:29:35 +0800

    Sun Mar 19 01:29:35 CST 2023 test by bot.

---
+++ build-r4se.1-build-env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+DOCKERFILE=Dockerfile.r4se.1-build-env
+REGISTRY=registry.cn-beijing.aliyuncs.com
+NAMESPACE=licsber
+REPOSITORY=github
+TAG=r4se.1-build-env
+
+IMAGE="$REGISTRY/$NAMESPACE/$REPOSITORY:$TAG"
+sudo docker build -t "$IMAGE" --file "$DOCKERFILE" .
+sudo docker push "$IMAGE"
+
--- build-r4se.2-rootfs.sh
+++ build-r4se.2-rootfs.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-DOCKERFILE=Dockerfile.r4se
+DOCKERFILE=Dockerfile.r4se.2-rootfs
 REGISTRY=registry.cn-beijing.aliyuncs.com
 NAMESPACE=licsber
 REPOSITORY=github
-TAG=r4se.manual
+TAG=r4se.2-rootfs
 
 IMAGE="$REGISTRY/$NAMESPACE/$REPOSITORY:$TAG"
 sudo docker build -t "$IMAGE" --file "$DOCKERFILE" .
