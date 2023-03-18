67e60d0c3282f08db6ed185d90558925f6b7602a
Jiale  Liu <licsber@gmail.com>
2023-03-19 00:49:22 +0800

    Create r4se_custom_config.sh

---
+++ openwrt-scripts/r4se_custom_config.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+sed -i -e '/CONFIG_MAKE_TOOLCHAIN=y/d' configs/rockchip/01-nanopi
+sed -i -e 's/CONFIG_IB=y/# CONFIG_IB is not set/g' configs/rockchip/01-nanopi
+sed -i -e 's/CONFIG_SDK=y/# CONFIG_SDK is not set/g' configs/rockchip/01-nanopi
