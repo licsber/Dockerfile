f3716328f7bbb25a9f9c1e3af7571194f76ac706
licsber <admin@licsber.site>
2023-03-19 13:35:37 +0800

    Sun Mar 19 13:35:37 CST 2023 test by bot.

---
+++ run-r4se.1-build-env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+sudo docker run --rm -it registry.cn-beijing.aliyuncs.com/licsber/github:r4se.1-build-env
+
--- run-r4se.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-sudo docker run --rm -it registry.cn-beijing.aliyuncs.com/licsber/github:r4se
-
