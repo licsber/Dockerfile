db8e1050f842770affb5ec44c140a1d209fc3427
licsber <admin@licsber.site>
2023-04-04 14:52:31 +0800

    2023年 04月 04日 星期二 14:52:31 CST test by bot.

--- run-r4se.1-build-env.sh
+++ run-r4se.1-build-env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-sudo docker run --rm -it registry.cn-beijing.aliyuncs.com/licsber/github:r4se.1-build-env
+docker run --rm -it registry.cn-beijing.aliyuncs.com/licsber/github:r4se.1-build-env
 
