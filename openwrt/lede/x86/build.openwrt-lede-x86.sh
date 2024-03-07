fc631aa8a431c76310dfabe4a065489999d46be0
licsber <admin@licsber.site>
2024-03-08 06:44:20 +0800

    change dir when build.

--- openwrt/lede/x86/build.openwrt-lede-x86.sh
+++ openwrt/lede/x86/build.openwrt-lede-x86.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-DOCKERFILE=Dockerfile.openwrt-lede-env
+DOCKERFILE=Dockerfile.openwrt-lede-x86
 REGISTRY=registry.cn-beijing.aliyuncs.com
 NAMESPACE=licsber
 REPOSITORY=github
-TAG=openwrt-lede-env
+TAG=openwrt-lede-x86
 
 IMAGE="$REGISTRY/$NAMESPACE/$REPOSITORY:$TAG"
 sudo docker build -t "$IMAGE" --file "$DOCKERFILE" .
